9c961006ccba2b30b21a018423f80847b5a2ac2b
Cherezchur <49197652+Cherezchur@users.noreply.github.com>
2021-06-06 23:23:56 +0300

    подготовлена структура для создиния объектов

--- js/main.js
+++ js/main.js
@@ -1,6 +1,69 @@
-const getRandomInteger = (firstInteger, lastInteger) => firstInteger > lastInteger || firstInteger === lastInteger || firstInteger < 0 ? 'Введите корректные значения диапазона: положительные целые числа, включая 0, где второе число строго больше первого' : Math.floor(Math.random() * (lastInteger - firstInteger + 1) + firstInteger);
+const getRandomInteger = (firstInteger, lastInteger) => {
+  const lower = Math.ceil(Math.min(Math.abs(firstInteger), Math.abs(lastInteger)));
+  const upper = Math.floor(Math.max(Math.abs(firstInteger), Math.abs(lastInteger)));
+
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
 
 const getCommentValidity = (comment, maxSymbols) =>  comment.length <= maxSymbols;
 
-getRandomInteger(2,4);
-getCommentValidity('строка', 10);
+// функция для создания 25 объектов
+
+const NUMBER_OF_OBJECT = 25;
+const COMMENTS = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра.',
+  'В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают.',
+  'Как можно было поймать такой неудачный момент?!',
+];
+const NAMES = [
+  'Артем',
+  'Andrey',
+  'Stan',
+  'Mary',
+  'Rodion',
+  'Dariya',
+  'Gleb',
+  'Tihon',
+]
+
+const createFotoDescription = () => {
+  return {
+    id: getRandomInteger(1, 25),
+    url: 'photos/' + getRandomInteger(1, 25),
+    description: 'Описание для фотографии',
+    likes: getRandomInteger(15, 200),
+    comments: {
+      id: getRandomInteger(1, 25),
+      avatar: 'img/avatar-' + getRandomInteger(1, 6) + '.svg',
+      message: ,
+      name: ,
+    }
+  };
+};
+
+createFotoDescription();
+
+// id 1-25
+
+// url, строка — адрес картинки вида photos/{{i}}.jpg, где {{i}} — это число от 1 до 25. Адреса картинок не должны повторяться.
+
+// description, строка — описание фотографии. Описание придумайте самостоятельно.
+
+// likes, число — количество лайков, поставленных фотографии. Случайное число от 15 до 200.
+
+// comments, массив объектов — список комментариев, оставленных другими пользователями к этой фотографии.
+// Количество комментариев к каждой фотографии вы определяете на своё усмотрение.
+// Все комментарии генерируются случайным образом. Пример описания объекта с комментарием:
+
+/* {
+  id: 135,
+  avatar: 'img/avatar-6.svg',
+  message: 'В целом всё неплохо. Но не всё.',
+  name: 'Артём',
+} */
