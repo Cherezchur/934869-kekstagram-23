60799e7d240cbd7c32cec83e2d3936023e210304
Cherezchur <49197652+Cherezchur@users.noreply.github.com>
2021-06-17 00:17:27 +0300

    Исправлены ошибки ES-lint

--- js/mini-images.js
+++ js/mini-images.js
@@ -6,11 +6,11 @@ const pictureTemplate = document.querySelector('#picture').content.querySelector
 const pictureListFragment = document.createDocumentFragment();
 
 photoDescriptions.forEach(({url, likes, comments}) => {
-    const pictureElement = pictureTemplate.cloneNode(true);
-    pictureElement.querySelector('.picture__img').setAttribute('src', url);
-    pictureElement.querySelector('.picture__likes').textContent = likes;
-    pictureElement.querySelector('.picture__comments').textContent = comments.length;
-    pictureListFragment.appendChild(pictureElement);
-})
+  const pictureElement = pictureTemplate.cloneNode(true);
+  pictureElement.querySelector('.picture__img').setAttribute('src', url);
+  pictureElement.querySelector('.picture__likes').textContent = likes;
+  pictureElement.querySelector('.picture__comments').textContent = comments.length;
+  pictureListFragment.appendChild(pictureElement);
+});
 
 picturesContainer.appendChild(pictureListFragment);
