e7732b9cb81c96153621e54b2061abd1f3be6933
Cherezchur <49197652+Cherezchur@users.noreply.github.com>
2021-06-30 00:01:48 +0300

    В форму загрузки фотографии добавлен функционал масштабирования

--- js/form.js
+++ js/form.js
@@ -5,6 +5,10 @@ const newFilePopup = document.querySelector('.img-upload__overlay');
 const closeButtonNewFilePopup = newFilePopup.querySelector('.cancel');
 const hashTagField = newFilePopup.querySelector('.text__hashtags');
 const commendField = newFilePopup.querySelector('.text__description');
+const buttonSmaller = document.querySelector('.scale__control--smaller');
+const buttonBigger = document.querySelector('.scale__control--bigger');
+const uploudedImage = document.querySelector('.img-upload__preview');
+const scaleField = document.querySelector('.scale__control--value');
 
 uploadField.addEventListener('change', () => {
 
@@ -48,6 +52,48 @@ uploadField.addEventListener('change', () => {
     }
   });
 
+  const reducedImage = () => {
+    
+    switch (scaleField.value) {
+        case '100%': 
+            uploudedImage.style.transform = 'scale(0.75)';
+            buttonBigger.removeAttribute('disabled', 'disabled');
+            scaleField.value = '75%';
+            break;
+        case '75%':
+            uploudedImage.style.transform = 'scale(0.5)';
+            scaleField.value = '50%';
+            break;
+        case '50%':
+            uploudedImage.style.transform = 'scale(0.25)';
+            buttonSmaller.setAttribute('disabled', 'disabled');
+            scaleField.value = '25%';
+            break;
+    }
+
+  }
+
+  const largeImage = () => {
+
+      switch (scaleField.value) {
+          case '75%': 
+              uploudedImage.style.transform = 'scale(1)';
+              buttonBigger.setAttribute('disabled', 'disabled');
+              scaleField.value = '100%';
+              break;
+          case '50%':
+              uploudedImage.style.transform = 'scale(0.75)';
+              scaleField.value = '75%';
+              break;
+          case '25%':
+              uploudedImage.style.transform = 'scale(0.5)';
+              buttonSmaller.removeAttribute('disabled', 'disabled');
+              scaleField.value = '50%';
+              break;
+      }
+
+  }
+
   const onCloseNewFilePopup = (evt) => {
 
     const closeNewFilePopup = () => {
@@ -66,6 +112,8 @@ uploadField.addEventListener('change', () => {
 
       document.removeEventListener('keydown', onCloseNewFilePopup);
       closeButtonNewFilePopup.removeEventListener('click', closeNewFilePopup);
+      buttonBigger.removeEventListener('click', largeImage);
+      buttonSmaller.removeEventListener('click', reducedImage);
     };
 
     if (document.activeElement.className === 'img-upload__cancel  cancel') {
@@ -73,10 +121,12 @@ uploadField.addEventListener('change', () => {
     } else if (isEscEvent(evt)) {
       closeNewFilePopup();
     }
+
   };
 
   closeButtonNewFilePopup.addEventListener('click', onCloseNewFilePopup);
-
   document.addEventListener('keydown', onCloseNewFilePopup);
+  buttonBigger.addEventListener('click', largeImage);
+  buttonSmaller.addEventListener('click', reducedImage);
 });
 
